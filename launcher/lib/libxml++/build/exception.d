c99929581cb2f2a069b43d9bee79979513cee2b4
aaron <aaron@336e800a-23d8-41f8-b78d-e9b071a98a33>
2010-03-08 18:11:31 +0000

    Er, removed libxml* build files
    git-svn-id: svn://japaneatahand.com/rawksd/trunk@32 336e800a-23d8-41f8-b78d-e9b071a98a33

--- launcher/lib/libxml++/build/exception.d
+++
@@ -1,8 +0,0 @@
-exception.o:  \
- /home/aaron/projects/rawksd/svn/trunk/launcher/lib/libxml++/libxml++/exceptions/exception.cc \
-  /home/aaron/projects/rawksd/svn/trunk/launcher/lib/libxml++/libxml++/exceptions/exception.h \
-  /home/aaron/projects/rawksd/svn/trunk/launcher/lib/libxml++/libxml++/libxml++config.h
-
-/home/aaron/projects/rawksd/svn/trunk/launcher/lib/libxml++/libxml++/exceptions/exception.h:
-
-/home/aaron/projects/rawksd/svn/trunk/launcher/lib/libxml++/libxml++/libxml++config.h:
--- launcher/lib/libxml2/build/trionan.d
+++
@@ -1,8 +0,0 @@
-trionan.o:  \
- /home/aaron/projects/rawksd/svn/trunk/launcher/lib/libxml2/./trionan.c \
-  /home/aaron/projects/rawksd/svn/trunk/launcher/lib/libxml2/./triodef.h \
-  /home/aaron/projects/rawksd/svn/trunk/launcher/lib/libxml2/./trionan.h
-
-/home/aaron/projects/rawksd/svn/trunk/launcher/lib/libxml2/./triodef.h:
-
-/home/aaron/projects/rawksd/svn/trunk/launcher/lib/libxml2/./trionan.h:
