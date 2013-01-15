cbd83c891ae6646af81b50a3e3d50cd0420423ae
tueidj <tueidj@336e800a-23d8-41f8-b78d-e9b071a98a33>
2013-01-15 18:55:56 +0000

    make leaderboard code more stable
    git-svn-id: svn://japaneatahand.com/rawksd/trunk@236 336e800a-23d8-41f8-b78d-e9b071a98a33

--- rawksd/kamek/leaderboard.cpp
+++ rawksd/kamek/leaderboard.cpp
@@ -6,29 +6,52 @@
 #define LEADERBOARD_ENABLED() \
 	(*(u32*)0x80002FFC == 0x1337BAAD)
 
-static const char asciitohex[16] = "0123456789ABCDEF";
+const char asciitohex[16] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 
-static char* ToHttpString(const char* str, int length = 0)
+static char* ToHttpString(const char* str, int length = 256)
 {
-	if (!length)
-		length = strlen(str);
+	int newlength=1;
+	const char *s = str;
 	char *dest, *d;
-	dest = d = (char*)malloc(length * 3 + 1);
+	int c;
 
-	for (int i = 0; i < length; i++) {
-		char chr = str[i];
-		if ((chr >= 'a' && chr <= 'z') || (chr >= 'A' && chr <= 'Z') || (chr >= '0' && chr <= '9')) {
-			*d++ = chr;
-		} else {
-			*d++ = '%';
-			*d++ = asciitohex[(chr>>4)&0xF];
-			*d++ = asciitohex[chr&0xF];
+	if (str)
+	{
+		while (*s && length--)
+		{
+			c = *s;
+			if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+				newlength++;
+			else
+				newlength+=3;
+			s++;
 		}
 	}
 
-	*d = '\0';
+	length = s - str;
+
+	dest = (char*)malloc(newlength);
+	if (dest)
+	{
+		for (d = dest; length--; str++)
+		{
+			c = *str;
+			if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+				*d++ = *str;
+			else
+			{
+				*d++ = '%';
+				*d++ = asciitohex[c>>4];
+				*d++ = asciitohex[c&0xF];
+			}
+		}
+		*d = '\0';
+	}
+
 	return dest;
 }
+
+#define HOSTNAME     "rvlution.net"
 
 static bool SubmitLeaderboardRawkSD(Symbol* symbol, int instrument, int difficulty, int score)
 {
@@ -36,44 +59,95 @@ static bool SubmitLeaderboardRawkSD(Symbol* symbol, int instrument, int difficul
 		return false;
 	if (!ThePlatformMgr.IsConnected)
 		return false;
+	if (symbol==NULL || symbol->name==NULL)
+		return false;
 
-	const char* hostname = "rvlution.net";
 	char* username;
 	char* songname;
 	char* fullsongname;
 	char* artistname;
 	char* message = NULL;
-	u32 console = 0;
-	int socket = 0;
-	int ret = 0;
+	u32 console;
+	int socket;
+	int ret=0;
 	struct sockaddr_in address;
 	hostent* host;
-	int size = 0;
-	u32 checksum = 0;
+	int size;
+	u32 checksum;
 	const char* song = symbol->name;
 	const char* originalsongname = gSongMgrWii.SongName(symbol);
-	bool hasname = originalsongname && strlen(originalsongname);
+	const char* originalartistname = gSongMgrWii.SongArtist(symbol);
+	PassiveMessagesPanel* panel = GetPMPanel();
 
-	memset(&address, 0, sizeof(address));
 	address.sin_family = PF_INET;
 	address.sin_len = 8;
+	address.sin_port = 80;
 
 	// FIXME: should use the friend code instead of console ID to handle wii->wiiu migration
 	const char* fullusername = ThePlatformMgr.GetUsernameFull(); // Full username is in the format "RB2OnlineName;consolefriendcode"
-	char* colon = strrchr(fullusername, ';');
+	if (fullusername==NULL)
+	{
+		OSReport("RawkSD: Failed to get fullusername\n");
+		goto onerror;
+	}
+	const char* colon = strrchr(fullusername, ';');
 
 	if (ES_GetDeviceID(&console) < 0) {
 		OSReport("RawkSD: Error getting ID\n");
 		goto onerror;
 	}
 
-	host = net_gethostbyname(hostname);
+	// any of these may return NULL (out of memory)
+	username = ToHttpString(fullusername, colon ? (colon - fullusername) : 256);
+	songname = ToHttpString(song);
+	if (username==NULL || songname==NULL) {
+		OSReport("RawkSD: failed to http-convert username or songname\n");
+		free(username);
+		free(songname);
+		goto onerror;
+	}
+	// fullsongname and/or artistname are allowed to be NULL
+	fullsongname = ToHttpString(originalsongname);
+	artistname = ToHttpString(originalartistname);
+
+	checksum = instrument << 3;
+	checksum |= difficulty;
+	checksum ^= console >> 4;
+	checksum ^= ~score << 6;
+	for (u32 i = 0; i < strlen(song); i++)
+		checksum ^= (u32)song[i] << 24;
+
+	size = 0x100 + strlen(username) + strlen(songname) + (fullsongname ? strlen(fullsongname):0) + (artistname ? strlen(artistname):0) + strlen(HOSTNAME);
+	message = (char*)malloc(size);
+	if (message) {
+		sprintf(message, "GET /rb2/post?inst=%d&diff=%d&score=%d&console=%d&nickname=%s&songid=%s&valid=%d", instrument, difficulty, score, console, username, songname, checksum);
+		if (fullsongname) {
+			strcat(message, "&name=");
+			strcat(message, fullsongname);
+		}
+		if (artistname) {
+			strcat(message, "&artist=");
+			strcat(message, artistname);
+		}
+		strcat(message, " HTTP/1.1\r\nHost: " HOSTNAME "\r\n\r\n\r\n");
+	}
+
+	free(username);
+	free(songname);
+	free(fullsongname);
+	free(artistname);
+
+	if (message==NULL) {
+		OSReport("RawkSD: out of memory for http buffer\n");
+		goto onerror;
+	}
+
+	host = net_gethostbyname(HOSTNAME);
 	if (!host || host->h_length != sizeof(address.sin_addr) || host->h_addrtype != PF_INET || host->h_addr_list == NULL || host->h_addr_list[0] == NULL) {
 		OSReport("RawkSD: Host lookup failed.\n");
 		goto onerror;
 	}
 	memcpy(&address.sin_addr, host->h_addr_list[0], host->h_length);
-	address.sin_port = htons(80);
 
 	socket = net_socket(PF_INET, SOCK_STREAM, 0);
 	if (socket < 0) {
@@ -87,24 +161,6 @@ static bool SubmitLeaderboardRawkSD(Symbol* symbol, int instrument, int difficul
 		goto onerror;
 	}
 
-	username = ToHttpString(fullusername, colon ? (colon - fullusername) : 0);
-	songname = ToHttpString(song);
-	fullsongname = ToHttpString(originalsongname);
-	artistname = ToHttpString(gSongMgrWii.SongArtist(symbol));
-
-	checksum = instrument << 3;
-	checksum |= difficulty;
-	checksum ^= console >> 4;
-	checksum ^= ~score << 6;
-	for (u32 i = 0; i < strlen(song); i++)
-		checksum ^= (u32)song[i] << 24;
-
-	message = (char*)malloc(0x100 + strlen(username) + strlen(songname) + strlen(fullsongname) + strlen(artistname) + strlen(hostname));
-	sprintf(message, "GET /rb2/post?inst=%d&diff=%d&score=%d&console=%d&nickname=%s&songid=%s&name=%s&artist=%s&valid=%d HTTP/1.1\r\nHost: %s\r\n\r\n\r\n", instrument, difficulty, score, console, username, songname, fullsongname, artistname, checksum, hostname);
-	free(username);
-	free(songname);
-	free(fullsongname);
-	free(artistname);
 	size = strlen(message);
 	ret = net_send(socket, message, size, 0);
 	net_close(socket);
@@ -114,10 +170,10 @@ static bool SubmitLeaderboardRawkSD(Symbol* symbol, int instrument, int difficul
 	}
 
 	OSReport("RawkSD: Leaderboard submission for \"%s\" succeeded.\n", song);
-	if (!message)
-		message = (char*)malloc(0x100);
-	sprintf(message, "Your new high score%s%s been sent to the RawkSD Leaderboards!", hasname ? " for " : "", hasname ? originalsongname : "");
-	GetPMPanel()->QueueMessage(message);
+	if (panel) {
+		sprintf(message, "Your new high score%s%s been sent to the RawkSD Leaderboards!", originalsongname ? " for " : "", originalsongname ? originalsongname : "");
+		panel->QueueMessage(message);
+	}
 	free(message);
 	return true;
 
@@ -125,9 +181,15 @@ onerror:
 	OSReport("RawkSD: Leaderboard submission for \"%s\" failed! :(\n", song);
 	if (!message)
 		message = (char*)malloc(0x100);
-		sprintf(message, "An error occurred uploading your high score%s%s to RawkSD.", hasname ? " for " : "", hasname ? originalsongname : "");
-	GetPMPanel()->QueueMessage(message);
-	free(message);
+	if (message)
+	{
+		if (panel) {
+			sprintf(message, "An error occurred uploading your high score%s%s to RawkSD.", originalsongname ? " for " : "", originalsongname ? originalsongname : "");
+			GetPMPanel()->QueueMessage(message);
+		}
+		free(message);
+	}
+
 	return false;
 }
 
